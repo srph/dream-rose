f6e910424b724b17357985e097125bf671c22870
SeraphiPod <seraphipod@gmail.com>
2014-04-29 04:27:57 +0800

    Improved logging algorithm

--- app/models/VoteLog.php
+++ app/models/VoteLog.php
@@ -25,7 +25,6 @@ class VoteLog extends Eloquent {
 	 */
 	public $timestamps = true;
 
-
 	/**
 	 * Verifies if the vote is valid
 	 *
@@ -33,74 +32,89 @@ class VoteLog extends Eloquent {
 	 * @param 	string 	 	$ip
 	 * @return 	boolean
 	 */
-	public static function validate(User $user, $id, $ip)
+	public static function validate(User $user, VoteLink $link, $ip)
 	{
 		$user->load('logs');
-		// First, it looks for whether the user has logs.
-		// Then, it checks if the ip of the current request matches
-		// the latest log.
-		// Otherwise, it will automatically return true.
-		if( $user->logs->count() ) {
 
-			$ipWithUserExists = $user->logs()
-				->where('ip', $ip)
-				->get()
-				->count();
+		// Checks if the ip of the current request exists in the logs.
+		// Then validates if the rule exists.
+		// Otherwise, checks if the user has a log in the last 24 hours,
+		// and validates.
 
-			if( $ipWithUserExists ) {
-
-				$log = $user->logs()					
-					->where('vote_link_id', $id)
-					->where('ip', $ip)
-					->orderBy('id', 'desc')
-					->first();
-
-				if( !empty($log) ) return $log->intervalValid();
-
-			} else {
-
-				$log = $user->logs()
-					->where('vote_link_id', $id)
-					->orderBy('id', 'desc')
-					->first();
-
-				if ( !empty($log) ) return $log->intervalValid();
-
+		if ( $this->checkLastIPLog($link, $ip) ) {
+			if ( ! $this->intervalValid($log) ) {
+				return false;
 			}
-		} else {
+		}
 
-			$log = self::where('ip', $ip)
-				->where('vote_link_id', $id)
-				->orderBy('id', 'desc')
-				->first();
-
-			if( !empty($log) ) return $log->intervalValid();
-
+		if( $user->logs->count() ) {
+			if( $this->checkLastUserLog($user, $link->id) ) {
+				if ( ! $this->intervalValid($log) ) {
+					return false;
+				}
+			}
 		}
 
 		return true;
 	}
 
-
 	/**
-	 * {{}}
+	 * Verifes if the required interval is valid
 	 *
 	 * @return 	boolean
 	 */
-	public function intervalValid()
+	public function intervalValid($log)
 	{
-		$config 		= Config::get('dream.links');
-		$interval 		= $config['interval'];
-		$db 			= strtotime( $this->created_at );
-		$now 			= Carbon::now();
-		$logged 		= Carbon::createFromTimestamp( $db );
+		$interval = Config::get('dream.links.interval');
+		$log 	= strtotime( $log->created_at );
+		$now 	= Carbon::now();
+		$logged = Carbon::createFromTimestamp( $log );
 
-		if ( $logged->diffInHours($now) >= 12 ) {
+		if ( $logged->diffInHours($now) >= $interval ) {
 			return true;
 		}
 
 		return false;
 	}
+
+	/**
+	 * Checks the logs if the IP exists.
+	 *
+	 * @param 	VoteLink 	$link
+	 * @param 	string 		$ip
+	 * @return 	VoteLog|bool
+	 */
+	public function checkLastIPLog(VoteLink $link, $ip)
+	{
+		$log = $link->logs()
+			->where('ip', $ip)
+			->orderBy('created_at', 'desc')
+			->first();
+
+		if ( is_null($log) ) return false;
+
+		return $ip
+	}
+
+	/**
+	 * Checks the user's last log on the the vote link
+	 *
+	 * @param 	User 		$user
+	 * @param 	VoteLink 	$link
+	 * @return 	VoteLog|bool
+	 */
+	public function checkLastUserLog(User $user, VoteLink $link)
+	{
+		$log = $user->logs()
+			->where('vote_link_id', $link->id)
+			->orderBy('created_at', 'desc')
+			->first();
+
+		if ( is_null($log) ) return false;
+
+		return $log;
+	}
+
 
 	/*
 	|--------------------------------------------------------------------------
@@ -115,7 +129,17 @@ class VoteLog extends Eloquent {
 	 */
 	public function user()
 	{
-		return $this->belongsTo('user');
+		return $this->belongsTo('User');
+	}
+
+	/**
+	 * ORM with the [Link] table
+	 *
+	 * @return 	Link
+	 */
+	public function links()
+	{
+		return $this->belongsTo('VoteLink');
 	}
 
 }
