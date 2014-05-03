edc7190689808090252e44bcd1d532c5aa6aa0a6
SeraphiPod <seraphipod@gmail.com>
2014-05-03 09:38:27 +0800

    Front page update.

--- app/views/pages/home/index.blade.php
+++ app/views/pages/home/index.blade.php
@@ -6,19 +6,25 @@
 	<h4> News </h4>
 	<hr>
 	<div class="row">
-		@foreach($news as $article)
-			<div class="col-md-4">
-				<div class="thumbnail" style="height: 290px;">
-					<img src="{{ $article->getImageURL() }}" width="238" height="162">
+		@if( $news->count() )
+			@foreach($news as $article)
+				<div class="col-md-4">
+					<div class="thumbnail" style="height: 290px;">
+						<img src="{{ $article->getImageURL() }}" width="238" height="162">
 
-					<div class="caption">
-						<h5> <strong> {{ $article->title }} </strong> </h5>
+						<div class="caption">
+							<h5> <strong> {{ $article->title }} </strong> </h5>
 
-						<p> {{ $article->summary }} </p>
+							<p> {{ $article->summary }} </p>
+						</div>
 					</div>
 				</div>
+			@endforeach
+		@else
+			<div class="alert alert-info">
+				<p> No <strong>news</strong> has been posted yet. Come back again for announcements! </p>
 			</div>
-		@endforeach
+		@endif
 	</div>
 
 	<div class="row">
@@ -26,20 +32,26 @@
 			<h4> Updates </h4>
 			<hr>
 			<div class="thumbnail">
-				<img src="{{ $updates[0]->getImageURL() }}" width="387" height="162">
+				@if( $updates->count() )
+					@foreach($updates as $update)
+						@if($index == 0)
+							<img src="{{ $updates[0]->getImageURL() }}" width="387" height="162">
 
-				<div class="caption">
-					<h5> <strong> {{ $updates[0]->title }} </strong> </h5>
-					<p> {{ $updates[0]->summary }} </p>
-				</div>
-
-				<ul class="nav nav-pills nav-stacked">
-					@foreach($updates as $index => $article)
-						@if($index > 0)
-							<li class="active"><a href="{{ URL::to('news/' . $article->id) }}"> {{ $article->title }} </a></li>
+							<div class="caption">
+								<h5> <strong> {{ $update->title }} </strong> </h5>
+								<p> {{ $update->summary }} </p>
+							</div>
+						@else
+							<ul class="nav nav-pills nav-stacked">
+								<li class="active"><a href="{{ URL::to('news/' . $article->id) }}"> {{ $update->title }} </a></li>
+							</ul>
 						@endif
-					@endforeach
-				</ul>
+					@endforach
+				@else
+					<div class="alert alert-info">
+						<p> No <strong>updates</strong> has been posted yet. Come back again for announcements! </p>
+					</div>
+				@endif
 			</div>
 		</div>
 
@@ -47,20 +59,26 @@
 			<h4> Events </h4>
 			<hr>
 			<div class="thumbnail">
-				<img src="{{ $updates[0]->getImageURL() }}" width="387" height="162">
+				@if( $events->count() )
+					@foreach($events as $index => event)
+						@if($index == 0)
+							<img src="{{ $updates[0]->getImageURL() }}" width="387" height="162">
 
-				<div class="caption">
-					<h5> <strong> {{ $events[0]->title }} </strong> </h5>
-					<p> {{ $events[0]->summary }} </p>
-				</div>
-
-				<ul class="nav nav-pills nav-stacked">
-					@foreach($events as $index => $article)
-						@if($index > 0)
-							<li class="active"><a href="{{ URL::to('news/' . $article->id) }}"> {{ $article->title }} </a></li>
+							<div class="caption">
+								<h5> <strong> {{ $event->title }} </strong> </h5>
+								<p> {{ $event->summary }} </p>
+							</div>
+						@else
+							<ul class="nav nav-pills nav-stacked">
+								<li class="active"><a href="{{ URL::to('news/' . $article->id) }}"> {{ $event->title }} </a></li>
+							</ul>
 						@endif
 					@endforeach
-				</ul>
+				@else
+					<div class="alert alert-info">
+						<p> No <strong>updates</strong> has been posted yet. Come back again for announcements! </p>
+					</div>
+				@endif
 			</div>
 		</div>
 	</div>
