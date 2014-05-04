7215174ed8f0442d85c4199153d5d3e3732b8246
Kier Borromeo <seraphipod@gmail.com>
2014-05-04 11:44:44 +0000

    character.blade.php edited online with Bitbucket

--- app/views/pages/home/ranking/character.blade.php
+++ app/views/pages/home/ranking/character.blade.php
@@ -6,27 +6,33 @@
 	<h1> Top 10 Characters </h1>
 	<hr>	
 
-	<table class="table">
-		<thead>
-			<tr>
-				<td> Rank </td>
-				<td> Name </td>
-				<td> Level </td>
-				<td> Job </td>
-				{{-- <td> {{ Clan </td> --}}
-			</tr>
-		</thead>
-
-		<tbody>
-			@foreach($characters as $index => $character)
+	@if( ! $characters->count() )
+		<div class="alert alert-info">
+			<p> No characters exist in the game yet! </p>
+		</div>
+	@else
+		<table class="table">
+			<thead>
 				<tr>
-					<td> {{ $index + 1 }} </td>
-					<td> {{ $character->name }} </td>
-					<td> {{ $character->level }} </td>
-					<td> {{ $character->getJobName() }} </td>
-					{{-- <td> {{ $character->clan }} </td> --}}
+					<td> Rank </td>
+					<td> Name </td>
+					<td> Level </td>
+					<td> Job </td>
+					{{-- <td> {{ Clan </td> --}}
 				</tr>
-			@endforeach
-		</tbody>
-	</table>
+			</thead>
+	
+			<tbody>
+				@foreach($characters as $index => $character)
+					<tr>
+						<td> {{ $index + 1 }} </td>
+						<td> {{ $character->name }} </td>
+						<td> {{ $character->level }} </td>
+						<td> {{ $character->getJobName() }} </td>
+						{{-- <td> {{ $character->clan }} </td> --}}
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	@endif
 @stop
