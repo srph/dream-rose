95fbf1db6219449375f2a17e335d59d473bf1783
Kier Borromeo <seraphipod@gmail.com>
2014-05-04 12:53:05 +0000

    HomeController.php edited online with Bitbucket

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -110,7 +110,12 @@ class HomeController extends BaseController {
 	public function getPlayerRanking()
 	{
 		if( !Cache::has('characters.ranking') ) {
-			$characters = $this->character->orderBy('btLEVEL', 'desc')->get();
+			$characters = $this->character
+				->whereHas(function($query) {
+					$query->where('Right', 1);
+				})
+				->orderBy('btLEVEL', 'desc')
+				->get();
 			$expiration = Carbon::now()->addMinutes(10);
 			Cache::add('characters.ranking', $characters, $expiration);
 		}
