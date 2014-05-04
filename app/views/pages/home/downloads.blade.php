c0ac8dbf8b130fd5658f72da71351683b6b0c69b
Kier Borromeo <seraphipod@gmail.com>
2014-05-04 05:41:59 +0000

    downloads.blade.php edited online with Bitbucket

--- app/views/pages/home/downloads.blade.php
+++ app/views/pages/home/downloads.blade.php
@@ -7,10 +7,10 @@
 	<hr>
 	
 	<div class="text-center">
-		<button type="button" class="btn btn-info btn-lg">
+		<a href="{{ url( Config::get('dream.downloads.client') ) }}" class="btn btn-info btn-lg">
 			<i class="glyphicon glyphicon-cloud"></i>
 			Download Client
-		</button>
+		</a>
 	</div>
 	<p></p>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget elit porttitor, venenatis eros eget, auctor elit. Ut consequat auctor velit eu porta. Nulla laoreet, purus viverra tincidunt facilisis, est nunc faucibus nibh, tincidunt feugiat arcu nisi nec ligula. Pellentesque pretium in lacus non condimentum. Etiam elementum lacinia nulla. Sed ut placerat massa, ac sollicitudin dui. Praesent venenatis neque ut velit sollicitudin, nec rutrum massa luctus. Cras in egestas lacus, vel egestas ligula. Sed porttitor lorem fermentum, volutpat odio et, dignissim nisl. </p>
