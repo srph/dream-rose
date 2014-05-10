294a51f48a6d516def7fe73287adbcd704931909
srph <seraphipod@gmail.com>
2014-05-10 11:00:31 +0800

    Removed unnecessary classes used in the recently added filter.

--- app/Dream/Filters/SSLFilter.php
+++ app/Dream/Filters/SSLFilter.php
@@ -1,27 +1,26 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Routing\Redirector as Redirect;
 
 class SSLFilter {
 
 	/**
 	 * Instance of SSL Filter
+	 *
 	 * @param Request 	$request
-	 * @param Response 	$response
 	 * @param Redirect 	$redirect
 	 */
-	public function __construct(Response $response, Redirect $redirect, Request $request)
+	public function __construct(Redirect $redirect, Request $request)
 	{
 		$this->request 	= $request;
-		$this->response = $response;
 		$this->redirect = $redirect;
 	}
 
 	/**
 	 * Force a route to be served on HTTPS
 	 *
+	 * @return 	void|Redirect
 	 */
 	public function filter()
 	{
