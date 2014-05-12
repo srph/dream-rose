6abadca489ad4f2246233dd91a2dd5248ecb29bd
Kier Borromeo <seraphipod@gmail.com>
2014-05-12 15:25:25 +0000

    Added FB Fanpage posts

--- app/views/templates/modules/side/facebook.blade.php
+++ app/views/templates/modules/side/facebook.blade.php
@@ -2,6 +2,6 @@
 	<div class="panel-heading"> Facebook </div>
 
 	<div class="panel-body">
-		<div class="fb-like-box" data-href="{{ Config::get('dream.api.facebook.url') }}" data-width="230" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+		<div class="fb-like-box" data-href="{{ Config::get('dream.api.facebook.url') }}" data-width="230" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="true" data-show-border="false"></div>
 	</div>
 </div>
