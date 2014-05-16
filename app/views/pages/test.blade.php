fac7078a1462445b317ab440783856297aaff911
Kier Borromeo <seraphipod@gmail.com>
2014-05-16 05:35:53 +0000

    test.blade.php created online with Bitbucket

---
+++ app/views/pages/test.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+
+<body>
+	{{ Form::open() }}
+		<input type="text" name="lalala" pogi="kier">
+		<input type="submit" value="submit">
+	{{ Form::close() }}
+</body>
+</html>
