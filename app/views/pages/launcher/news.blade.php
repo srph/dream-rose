d4abf87ed1744211d87d93c567be8c1e8e9376a2
SeraphiPod <seraphipod@gmail.com>
2014-05-28 08:58:21 +0800

    Updated launcher news template.

--- app/views/pages/launcher/news.blade.php
+++ app/views/pages/launcher/news.blade.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		{{ HTML::style('vendor/bootstrap-dist/css/bootstrap.min.css') }}
+		<style>
+			.container {
+				width: 100%;
+			}
+		</style>
 	</head>
 
 	<body>
 		<div class="container">
-			<div class="table table-hover">
+			<table class="table table-hover">
 				<thead>
 					<tr>
 						<td> # </td>
 						<td> Title </td>
+						<td> Type </td>
 						<td> Date </td>
 					</tr>
 				</thead>
@@ -19,11 +26,14 @@
 						<tr>
 							<td> {{ $article->id }} </td>
 							<td> {{ $article->title }} </td>
+							<td> {{ $article->category->name }} </td>
 							<td> {{ $article->created_at->diffForHumans }} </td>
 						</tr>
 					@endforeach
 				</tbody>
-			</div>
+			</table>
+
+			<div class="form-group"> {{ $news->links() }} </div>
 		</div>
 	</body>
 </html>
